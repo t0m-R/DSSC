08af4516ce82e0d931bc832b9e71f758f1dabbb9
Ivan Girotto <igirotto@ictp.it>
2019-03-28 18:49:59 +0100

    Fixed lab2

---
+++ Lab/Day2/loop_schedule.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void print_usage( int * a, int N, int nthreads ) {
+
+  int tid, i;
+  for( tid = 0; tid < nthreads; ++tid ) {
+
+    fprintf( stdout, "%d: ", tid );
+
+    for( i = 0; i < N; ++i ) {
+
+      if( a[ i ] == tid) fprintf( stdout, "*" );
+      else fprintf( stdout, " ");
+    }
+    printf("\n");
+  }
+}
+
+int main( int argc, char * argv[] ) {
+
+  // TODO                                                                                                                                                                                                                                                                                
+
+  return 0;
+}
